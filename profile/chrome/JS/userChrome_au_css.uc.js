3776884b537c903646f160ef0e0baea257e632ef
MrOtherGuy <jastekken@outlook.com>
2021-05-17 13:07:28 +0300

    Make userChrome_au_css.ic.js only load the sheet into the same document

--- profile/chrome/JS/userChrome_au_css.uc.js
+++ profile/chrome/JS/userChrome_au_css.uc.js
@@ -2,20 +2,29 @@
 // @name           userChrome_author_css
 // @namespace      userChrome_Author_Sheet_CSS
 // @version        0.0.5
-// @description    Load userChrome.au.css file as author sheet from resources folder using chrome: uri
+// @description    Load userChrome.au.css file as author sheet from resources folder using chrome: uri. The file is loaded only into the document where this script runs which by default is browser.xhtml
 // @onlyonce
+// @startup        preloadedAuthorSheet
 // ==/UserScript==
 
 (function () {
-
+  // define a startup object for the script loader to execute
+  // wherever this script is loaded
+  _ucUtils.sharedGlobal.preloadedAuthorSheet = {
+    sheet: null,
+    _startup: function(win){
+      if(!win || !this.sheet){
+        return
+      }
+      win.windowUtils.addSheet(this.sheet,Ci.nsIDOMWindowUtils.AGENT_SHEET);
+    }
+  };
+  // The next things execute only once per sessions because of @onlyonce in the header
 	let sss = Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService);
-  
-  // Try to load userChrome.au.css as author sheet
-  // WARNING - author sheets loaded like this affect each and every document you load including web sites. So be careful with your custom styles.
-  
   try{
-    sss.loadAndRegisterSheet(makeURI("chrome://userChrome/content/userChrome.au.css"), sss.AUTHOR_SHEET);
+    // Try to preload the file and save it to global area
+    _ucUtils.sharedGlobal.preloadedAuthorSheet.sheet = sss.preloadSheet(makeURI("chrome://userChrome/content/userChrome.au.css"), sss.AUTHOR_SHEET);
   }catch(e){
-    console.error(`Could not load userChrome.au.css: ${e.name}`)
+    console.error(`Could not pre-load userChrome.au.css: ${e.name}`)
   }
 })();
