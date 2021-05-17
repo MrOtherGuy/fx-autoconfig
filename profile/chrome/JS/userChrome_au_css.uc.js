5cf66f6453f759775694a5d5941ec197ab166942
MrOtherGuy <jastekken@outlook.com>
2021-05-17 12:00:40 +0300

    Change descriptions to reflect the fact that author sheets apply globally

--- profile/chrome/JS/userChrome_au_css.uc.js
+++ profile/chrome/JS/userChrome_au_css.uc.js
@@ -1,14 +1,18 @@
 // ==UserScript==
-// @name           userChrome_au_css.uc.js
-// @namespace      userChrome_Agent_Sheet_CSS
+// @name           userChrome_author_css
+// @namespace      userChrome_Author_Sheet_CSS
 // @version        0.0.5
-// @description    Load userChrome.au.css file as author sheet into the document that loads this script. The file is loaded from resources folder using chrome: uri
+// @description    Load userChrome.au.css file as author sheet from resources folder using chrome: uri
 // @onlyonce
 // ==/UserScript==
 
 (function () {
 
 	let sss = Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService);
+  
+  // Try to load userChrome.au.css as author sheet
+  // WARNING - author sheets loaded like this affect each and every document you load including web sites. So be careful with your custom styles.
+  
   try{
     sss.loadAndRegisterSheet(makeURI("chrome://userChrome/content/userChrome.au.css"), sss.AUTHOR_SHEET);
   }catch(e){
