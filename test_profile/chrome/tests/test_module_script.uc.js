96133e185498a27532cd29a67da8eeae9723e7d9
MrOtherGuy <jastekken@outlook.com>
2022-06-10 15:41:38 +0300

    add exported symbols to test_module_script

--- test_profile/chrome/tests/test_module_script.uc.js
+++ test_profile/chrome/tests/test_module_script.uc.js
@@ -2,14 +2,16 @@
 // @name           test_module_script
 // @backgroundmodule
 // ==/UserScript==
-
+let EXPORTED_SYMBOLS = [];
 let is_success = false;
 const x = 23;
 try{
   let x = window;
 }catch(e){
-  console.log(e);
   is_success = true;
 }
-
-console.log("test_module_script passed: "+  is_success);
+if(is_success){
+  console.log("test_module_script: OK");
+}else{
+  console.warn("test_module_script: expected failure got: success")
+}
